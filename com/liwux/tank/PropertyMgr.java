06cd0128562f706f6a28f8ab092fec890f5156b9
liwux <xix0.618@gmail.com>
2022-08-22 20:48:49 +0800

    测试读取配置

---
+++ com/liwux/tank/PropertyMgr.java
@@ -0,0 +1,27 @@
+package com.liwux.tank;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyMgr {
+    static Properties props = new Properties();
+
+    static {
+        try {
+            props.load(PropertyMgr.class.getClassLoader().getResourceAsStream("config"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Object get(String key){
+        if (props == null) return null;
+        return props.get(key);
+    }
+
+
+    public static void main(String[] args) {
+        //System.out.println(PropertyMgr.get("initTankCount"));
+    }
+
+}
