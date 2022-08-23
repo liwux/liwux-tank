379e3346c5aa7b6a284260e3e094336eb6097710
liwux <xix0.618@gmail.com>
2022-08-23 23:44:44 +0800

    strategy model lambda写法

--- com/liwux/strategy/Comparator.java
+++ com/liwux/strategy/Comparator.java
@@ -1,5 +1,10 @@
 package com.liwux.strategy;
 
+@FunctionalInterface
 public interface Comparator<T> {
     int Compare(T o1,T o2);
+
+    default void main(){
+        System.out.println("m");
+    }
 }
