875649b19a5f0963d2922313ebaf625caf61501f
liwux <xix0.618@gmail.com>
2022-08-27 18:01:40 +0800

    关闭decorator pattern

--- com/liwux/tank/strategy/DefualtFire.java
+++ com/liwux/tank/strategy/DefualtFire.java
@@ -12,7 +12,7 @@ public class DefualtFire implements FireStrategy {
     public void fire(Tank tank) {
         int bX = tank.x + Tank.tankWidth/2 - Bullet.bulletWidth/2;
         int bY = tank.y + Tank.tankHeight/2 - Bullet.bulletHeight/2;
-        GameModel.getInstance().add(new RectDecorator(new Bullet(bX,bY,tank.dir,tank.group)));
+        new RectDecorator(new Bullet(bX,bY,tank.dir,tank.group));
 
         //new Bullet(bX,bY,tank.dir,tank.group);
         new Thread(()->new Audio("audio/tank_fire.wav").play()).start();
